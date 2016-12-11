9f4699eefd9280cd98eb18336e29ff448f5431f6
Michał Michalski <github@michalski.im>
2016-12-11 19:06:19 +0000

    Bugfixes in Formatter

--- src/main/scala/im/michalski/golgifbot/formatters/WykopBlogFormatter.scala
+++ src/main/scala/im/michalski/golgifbot/formatters/WykopBlogFormatter.scala
@@ -9,9 +9,10 @@ class WykopBlogFormatter extends Formatter {
   val botOwner = "@Regis86"
   val botInfo =
     s"""**WPIS TESTOWY!**
-       |To jest bot użytkownika $botOwner wrzucający gole z Reddita
+       |To jest / będzie bot użytkownika $botOwner wrzucający gole z Reddita
        |Dodaj do czarnej listy tag ${tags.mkString(",")}, jeśli nie chcesz oglądać tych wpisów
-       |Docelowo, jeśli bot się sprawdzi i nie będzie robił głupich rzeczy, wpisy będąwrzucane pod tag # golgif automatycznie""".stripMargin
+       |Docelowo, jeśli bot się sprawdzi i nie będzie robił głupich rzeczy, wpisy będą wrzucane pod tag #golgif automatycznie
+       |(a póki co, zanim zintegruję się z API Wykopu, wrzucam ręcznie to co pobiorę :D )""".stripMargin
 
   private def printLine(maybeTime: Option[String], link: Link) = maybeTime match {
     case Some(time) if !link.description.contains(time) => s"$time ${link.description}: ${link.link}"
@@ -20,7 +21,7 @@ class WykopBlogFormatter extends Formatter {
 
   private def printEvent(event: MatchEvent) = event match {
     // FIXME: distinguish between goals and other events with links
-    case event: Goal => s"${event.links.map(link => printLine(event.time, link)).mkString("|")}\n"
+    case event: Goal => s"${event.links.map(link => printLine(event.time, link)).mkString(" | ")}\n"
     case _ => ""
   }
 
